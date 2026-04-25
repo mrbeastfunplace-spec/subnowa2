9f55af89dad19c7e10393848d83e0f0252964e25
Donjik <mrbeastfunplace@gmail.com>
2026-04-25 21:53:53 +0500

    donjik

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ else:
 # CONFIG
 # =========================================================
 
-BOT_TOKEN = "8640666329:AAEvZDhBO72GVrnZI37y8bG3PRpV3yvNCGE"
+BOT_TOKEN = "8640666329:AAEuuWjXQKlk7Q3FME1CrMecsc0rl5eiMWA"
 
 ADMIN_IDS = [7716923294]
 
